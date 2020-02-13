316d7d63d6c5fa2a0494ef746ae974089d24cea3
Roderick Azzopardi <Roderick.Azzopardi.b43256@mcast.edu.mt>
2020-02-13 10:45:47 +0100

    Test

---
+++ WebApplication_62B/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplication_62B.Startup))]
+namespace WebApplication_62B
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
